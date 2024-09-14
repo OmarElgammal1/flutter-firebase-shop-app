84ec7d1e5602a91c99cf8bec816b842845b52f64
OmarTawfiikk <omartawfik333@gmail.com>
2024-09-14 16:54:37 +0300

    Updated App Bars

--- shop_app/lib/bars.dart
+++ shop_app/lib/bars.dart
@@ -4,7 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,9 +27,8 @@ class MyApp extends StatelessWidget {
             child: IconButton(
               icon: Icon(Icons.menu, color: Colors.black),
               onPressed: () {
-                
+
                 // Handle button press
-                
               },
             ),
           ),
@@ -36,13 +42,43 @@ class MyApp extends StatelessWidget {
               child: IconButton(
                 icon: Icon(Icons.shopping_bag, color: Colors.black),
                 onPressed: () {
-                 
+
                   // Handle shopping bag
-                  
                 },
               ),
             ),
           ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline),
+              label: 'Wishlist',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined),
+              label: 'Cart',
+            ),
+             BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined),
+              label: 'Orders',
+            ),
+          ],
+          selectedItemColor: Colors.orange,
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
         ),
       ),
     );

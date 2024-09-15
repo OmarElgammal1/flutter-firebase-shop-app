23697c63afc91eb59cdf94bf594344e748e14945
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-09-15 12:33:47 +0300

    Connect home screen with product details

--- shop_app/lib/home_screen.dart
+++ shop_app/lib/home_screen.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'product_card.dart';
 import 'shop_app_bar.dart';
 import 'nav_bar.dart';
-import 'data.dart'; // Import the product data
+import 'data.dart';
+import 'product_details_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  final String screenType; // Pass 'Wishlist', 'Cart', etc.
+  final String screenType;
 
   HomeScreen({required this.screenType});
 
   @override
   Widget build(BuildContext context) {
-
-    var productList = products.values.toList();
+    var productList = products.entries.toList();
 
     return Scaffold(
       appBar: ShopAppBar(
@@ -21,11 +21,14 @@ class HomeScreen extends StatelessWidget {
       body: GridView.builder(
         itemCount: productList.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // items per row
+          crossAxisCount: 2,
         ),
         itemBuilder: (context, index) {
-          var product = productList[index];
+          var productEntry = productList[index];
+          var product = productEntry.value;
+
           return ProductCard(
+            id: productEntry.key,
             productName: product['productName'],
             imageUrl: product['imageUrls'][0],
             price: product['price'],
@@ -35,7 +38,6 @@ class HomeScreen extends StatelessWidget {
               print('${product['productName']} added to cart');
             },
             onRemoveFromWishlist: () {
-
               print('${product['productName']} removed from wishlist');
             },
           );
@@ -45,4 +47,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-

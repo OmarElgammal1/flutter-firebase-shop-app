aca3a0f91ed47d604153b7f4f6dad023e2e8028e
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-09-15 15:56:19 +0300

    Nav bar functionallity

--- shop_app/lib/nav_bar.dart
+++ shop_app/lib/nav_bar.dart
@@ -1,48 +1,39 @@
 import 'package:flutter/material.dart';
 
+class ShopNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTabSelected;
 
-
-class ShopNavBar extends StatefulWidget {
-  @override
-  _ShopNavBarState createState() => _ShopNavBarState();
-}
-
-class _ShopNavBarState extends State<ShopNavBar> {
-  int _currentIndex = 1;
+  ShopNavBar({required this.currentIndex, required this.onTabSelected});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline),
-              label: 'Wishlist',
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined),
-              label: 'Cart',
-            ),
-             const BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined),
-              label: 'Orders',
-            ),
-          ],
-          selectedItemColor: Colors.orange,
-          unselectedItemColor: Colors.grey,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          type: BottomNavigationBarType.fixed,
-        );
-
+      currentIndex: currentIndex,
+      onTap: onTabSelected,
+      items: [
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Wishlist',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_bag_outlined),
+          label: 'Cart',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.list_alt_outlined), // Updated icon for orders
+          label: 'Orders',
+        ),
+      ],
+      selectedItemColor: Colors.orange,
+      unselectedItemColor: Colors.grey,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+    );
   }
 }

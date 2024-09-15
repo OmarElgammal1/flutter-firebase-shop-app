2f2facf3615e2f293be935dfa6afa07e6c123877
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-09-15 15:56:43 +0300

    Update main

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'reviews_screen.dart';
-import 'add_review.dart';
-import 'home_screen.dart';
-import 'product_details_screen.dart';
-import 'wishlist_screen.dart';
+import 'main_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,11 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WishlistScreen(screenType: "Wishlist"),
-      //home: ProductDetailScreen(id: 1),
-      //home: HomeScreen(screenType: 'Home'), // Start with Wishlist or any screen type
-      //home: ReviewsScreen(id: 1),
-      //home: AddReviewScreen(),
+
+      home: MainScreen(),
     );
   }
 }
